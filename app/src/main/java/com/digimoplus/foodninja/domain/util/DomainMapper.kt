70b5018ae1b5513338c3d2a599010cfa46d73fb3
alireza momenpour <alirezamomenpour02@gmail.com>
2022-04-19 12:25:56 +0430

    add retrofit module and domain mapper

---
+++ app/src/main/java/com/digimoplus/foodninja/domain/util/DomainMapper.kt
@@ -0,0 +1,9 @@
+package com.digimoplus.foodninja.domain.util
+
+interface DomainMapper<T, DomainModel> {
+
+    fun mapToDomainModel(model: T): DomainModel
+
+    fun mapFromDomainModel(domainModel: DomainModel): T
+
+}
