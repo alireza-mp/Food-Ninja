a25c2c352fd1e09726b45e7845c17e4d15010d34
alireza_mo <alirezamomenpour01@gmail.com>
2022-05-03 12:16:46 +0430

    check is user information completed in splash page // save user information completed in sign in page

--- app/src/main/java/com/digimoplus/foodninja/presentation/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/digimoplus/foodninja/presentation/ui/splash/SplashFragment.kt
@@ -2,7 +2,6 @@ package com.digimoplus.foodninja.presentation.ui.splash
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,13 +18,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.digimoplus.foodninja.R
 import com.digimoplus.foodninja.presentation.components.GradientButton
-import com.digimoplus.foodninja.presentation.theme.AppTheme
 import com.digimoplus.foodninja.presentation.components.util.buttonGradient
+import com.digimoplus.foodninja.presentation.theme.AppTheme
 import com.digimoplus.foodninja.presentation.theme.isDark
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
@@ -42,7 +42,7 @@ class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
@@ -58,168 +58,169 @@ class SplashFragment : Fragment() {
             }
         }
     }
-}
 
-@Composable
-fun SplashPage(navController: NavController, viewModel: SplashViewModel, context: Context) {
+    @Composable
+    fun SplashPage(navController: NavController, viewModel: SplashViewModel, context: Context) {
 
-    val retryVisibility = remember { mutableStateOf(false) }
-    val coroutineScope = rememberCoroutineScope()
+        val retryVisibility = remember { mutableStateOf(false) }
+        val coroutineScope = rememberCoroutineScope()
 
-    LaunchedEffect(Unit) {
-        checkUserIntroduction(
-            viewModel = viewModel,
-            navController = navController,
-            retryVisibility = retryVisibility
-        )
-    }
+        LaunchedEffect(Unit) {
+            checkUserIntroduction(
+                viewModel = viewModel,
+                navController = navController,
+                retryVisibility = retryVisibility
+            )
+        }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colors.background),
-        contentAlignment = Alignment.Center,
-    ) {
-        Image(
-            modifier = Modifier.fillMaxSize(),
-            painter = painterResource(id = if (AppTheme.colors.isLight) R.drawable.background_light else R.drawable.background_dark),
-            contentScale = ContentScale.FillWidth,
-            contentDescription = ""
-        )
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center,
+        ) {
             Image(
-                modifier = Modifier
-                    .width(200.dp)
-                    .height(200.dp),
-                painter = painterResource(id = R.drawable.logo),
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = if (AppTheme.colors.isLight) R.drawable.background_light else R.drawable.background_dark),
+                contentScale = ContentScale.FillWidth,
                 contentDescription = ""
             )
-            Spacer(modifier = Modifier.padding(top = AppTheme.dimensions.grid_6))
-            if (!retryVisibility.value) {
-                CircularProgressIndicator(color = AppTheme.colors.primary)
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(200.dp),
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(top = AppTheme.dimensions.grid_6))
+                if (!retryVisibility.value) {
+                    CircularProgressIndicator(color = AppTheme.colors.primary)
+                } else {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = "Internet connection error...",
+                            style = AppTheme.typography.body,
+                            color = AppTheme.colors.titleText
+                        )
+                    }
+                }
+            }
+            if (retryVisibility.value) {
+                GradientButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = AppTheme.dimensions.grid_2),
+                    gradient = buttonGradient(),
+                    text = "Tap to try again...",
+                    textColor = Color.White
+                ) {
+                    coroutineScope.launch {
+                        retryVisibility.value = false
+                        checkUserIntroduction(
+                            viewModel = viewModel,
+                            navController = navController,
+                            retryVisibility = retryVisibility
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun checkUserIntroduction(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        viewModel.checkIntroduction.collect { state ->
+            if (state == "null") {
+                goToIntroductionPage(retryVisibility, viewModel, navController)
             } else {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        text = "Internet connection error...",
-                        style = AppTheme.typography.body,
-                        color = AppTheme.colors.titleText
-                    )
-                }
-            }
-        }
-        if (retryVisibility.value) {
-            GradientButton(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter).padding(bottom = AppTheme.dimensions.grid_2),
-                gradient = buttonGradient(),
-                text = "Tap to try again...",
-                textColor = Color.White
-            ) {
-                coroutineScope.launch {
-                    retryVisibility.value = false
-                    checkUserIntroduction(
-                        viewModel = viewModel,
-                        navController = navController,
-                        retryVisibility = retryVisibility
-                    )
-                }
+                checkUserAuthentication(retryVisibility, viewModel, navController)
             }
         }
     }
-}
 
-private suspend fun checkUserIntroduction(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-) {
-    viewModel.checkIntroduction.collect { state ->
-        if (state == "null") {
-            goToIntroductionPage(retryVisibility, viewModel, navController)
+    private suspend fun checkUserAuthentication(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        viewModel.checkAuthentication.collect { state ->
+            if (state == "null") {
+                goToSignUpPage(retryVisibility, viewModel, navController)
+            } else {
+                checkUserInformation(retryVisibility, viewModel, navController)
+            }
+        }
+    }
+
+    private suspend fun checkUserInformation(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        viewModel.checkUserInformation.collect { state ->
+            if (state == "null") {
+                goToUserInformationPage(retryVisibility, viewModel, navController)
+            } else {
+                goToHomePage(retryVisibility, viewModel, navController)
+            }
+        }
+
+    }
+
+    suspend fun goToUserInformationPage(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        delay(1500)
+        if (viewModel.isOnline()) {
+            navController.navigate(R.id.action_splashFragment_to_userInformationFragment)
         } else {
-            checkUserAuthentication(retryVisibility, viewModel, navController)
+            retryVisibility.value = true
         }
     }
-}
 
-private suspend fun checkUserAuthentication(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-) {
-    viewModel.checkAuthentication.collect { state ->
-        if (state == "null") {
-            goToSignUpPage(retryVisibility, viewModel, navController)
+    private suspend fun goToSignUpPage(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        delay(1500)
+        if (viewModel.isOnline()) {
+            navController.navigate(R.id.action_splashFragment_to_signUpFragment)
         } else {
-            checkUserInformation(retryVisibility, viewModel, navController)
-        }
-    }
-}
-
-private suspend fun checkUserInformation(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-){
-    viewModel.checkUserInformation.collect{state->
-        if (state == "null"){
-            goToUserInformationPage(retryVisibility, viewModel, navController)
-        }else{
-            goToHomePage(retryVisibility, viewModel, navController)
+            retryVisibility.value = true
         }
     }
 
-}
-
-suspend fun  goToUserInformationPage(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-) {
-    delay(1500)
-    if (viewModel.isOnline()) {
-        navController.navigate(R.id.action_splashFragment_to_userInformationFragment)
-    } else {
-        retryVisibility.value = true
+    private suspend fun goToHomePage(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        delay(1500)
+        if (viewModel.isOnline()) {
+            navController.navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            retryVisibility.value = true
+        }
     }
-}
 
-private suspend fun goToSignUpPage(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-) {
-    delay(1500)
-    if (viewModel.isOnline()) {
-        navController.navigate(R.id.action_splashFragment_to_signUpFragment)
-    } else {
-        retryVisibility.value = true
-    }
-}
-
-private suspend fun goToHomePage(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-) {
-    delay(1500)
-    if (viewModel.isOnline()) {
-        navController.navigate(R.id.action_splashFragment_to_homeFragment)
-    } else {
-        retryVisibility.value = true
-    }
-}
-
-private suspend fun goToIntroductionPage(
-    retryVisibility: MutableState<Boolean>,
-    viewModel: SplashViewModel,
-    navController: NavController
-) {
-    delay(1500)
-    if (viewModel.isOnline()) {
-        navController.navigate(R.id.action_splashFragment_to_introductionFragment)
-    } else {
-        retryVisibility.value = true
+    private suspend fun goToIntroductionPage(
+        retryVisibility: MutableState<Boolean>,
+        viewModel: SplashViewModel,
+        navController: NavController,
+    ) {
+        delay(1500)
+        if (viewModel.isOnline()) {
+            navController.navigate(R.id.action_splashFragment_to_introductionFragment)
+        } else {
+            retryVisibility.value = true
+        }
     }
 }
 
