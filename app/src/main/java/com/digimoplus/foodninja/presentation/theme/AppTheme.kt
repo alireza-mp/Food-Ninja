d4d09715b088a1ea45a21a5b279b2a840552e57d
alireza_mo <alirezamomenpour01@gmail.com>
2022-07-08 10:59:16 +0430

    change typography in appTheme

--- app/src/main/java/com/digimoplus/foodninja/presentation/theme/AppTheme.kt
+++ app/src/main/java/com/digimoplus/foodninja/presentation/theme/AppTheme.kt
@@ -1,11 +1,9 @@
 package com.digimoplus.foodninja.presentation.theme
 
-import android.util.Size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalConfiguration
 
 object AppTheme {
 
@@ -27,26 +25,15 @@ fun AppTheme(
     content: @Composable () -> Unit,
 ) {
 
-    val screenHeightDp = LocalConfiguration.current.screenHeightDp
     val rememberedColors = remember { colors.copy() }.apply { updateColorsFrom(colors) }
 
     CompositionLocalProvider(
         LocalColors provides rememberedColors,
-        LocalTypography provides getTypography(screenHeightDp)
+        LocalTypography provides AppTypography()
 
     ) {
         content()
     }
-}
-
-fun getTypography(height: Int): AppTypography {
-    /* return when {
-         height > 733 -> sw360DTypography
-         height in 641..732 -> mediumTypography
-         height < 640 -> smallTypography
-         else -> sw360DTypography
-     }*/
-    return sw360DTypography
 }
 
 fun isDark(state: Boolean): AppColors {
